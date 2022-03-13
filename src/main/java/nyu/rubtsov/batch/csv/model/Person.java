05462e068738c36a6ce34e3332841ad7eac9fcaf
Nikolai Rubtsov <nikolay1337@icloud.com>
2022-03-13 19:54:42 +0300

    Add Person model class

---
+++ src/main/java/nyu/rubtsov/batch/csv/model/Person.java
@@ -0,0 +1,37 @@
+package nyu.rubtsov.batch.csv.model;
+
+import java.util.Date;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private Date date;
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
